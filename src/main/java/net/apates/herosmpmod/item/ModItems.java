0de669dd4973c092d0bf209b6769924a06b44419
apates <papousekgames@gmail.com>
2025-11-22 20:13:55 +0100

    added some effects

--- src/main/java/net/apates/herosmpmod/item/ModItems.java
+++ src/main/java/net/apates/herosmpmod/item/ModItems.java
@@ -4,9 +4,15 @@ import net.apates.herosmpmod.HeroSmpMod;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -28,8 +34,44 @@ public class ModItems {
                 @Override
                 public boolean isBarVisible(ItemStack stack) {
                     stack.setDamageValue(-1);
-                    return false; // Hides the durability bar
+                    return false; // hides the durability bar
                 }
+                @Override
+
+                public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean selected) {
+                    super.inventoryTick(stack, level, entity, slot, selected);
+
+                    // jede to kazdej tick muze bejt performance problem
+                    //en: runs every single tick may be a performance problem
+
+                    // na servu
+                    //en: runs only on sever
+                    if (level.isClientSide) return;
+
+                    //I hate java why doesn´t it work without the instance I hate this shit
+                    if (entity instanceof Player player) {
+                        boolean isInMainHand = player.getMainHandItem() == stack;
+                        if(isInMainHand){
+                            player.addEffect(new MobEffectInstance(
+                                    MobEffects.FIRE_RESISTANCE,0,1
+                            ));
+                        }
+
+                    }
+                }
+
+                @Override
+                public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+                    //why did the change it to tick from seconds it makes it so much more difficul
+                    //to make goddammit
+                    //btw sets target on fire for 8 sec (second time ticks)
+                    //why are they changing everything it is so hard to find new tutorials
+                    //and the docs suck
+                    target.setRemainingFireTicks(8*20);
+
+                    return super.hurtEnemy(stack, target, attacker);
+                }
+
             });
 
 
